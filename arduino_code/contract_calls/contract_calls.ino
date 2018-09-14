afcf4f5f58af9356d0e59b7356e87d35c8fb513c
Rosen Krumov <rosenkrumov95@gmail.com>
2018-09-14 12:17:12 +0300

    Added working code for esp32 and added working web3-ganache

--- arduino_code/contract_calls/contract_calls.ino
+++ arduino_code/contract_calls/contract_calls.ino
@@ -2,31 +2,35 @@
 #include <Web3.h>
 #include <Contract.h>
 
+
+#define SERIAL_BUFFER_SIZE 100
 #define USE_SERIAL Serial
 
-#define ENV_SSID     "SoftuniHQ"
-#define ENV_WIFI_KEY "Room4all"
-#define MY_ADDRESS "0x70c9F6ABb74178fbD52c800F2D72Ab683F5F368B"
-#define CONTRACT_ADDRESS "0x7914b79992225a6a42a2640594d62faa69f61043"
-#define INFURA_HOST "ropsten.infura.io"
-#define INFURA_PATH "/7a2a37996d564ccd86812bd19d4001de"
+#define ENV_SSID     "Xperia Z3+_74db"
+#define ENV_WIFI_KEY "12345678"
+#define GANACHE_HOST "35.242.225.96"
+#define MY_ADDRESS "0x4b6a4d63684bca7582473d8d7dc7fb25d24a6442"
+#define CONTRACT_ADDRESS "0x1e4743ffd49942cf804050bd48d92c8875722d40"
 
-const char PRIVATE_KEY[] = {0xfd,0x47,0x09,0x81,0x6c,0x7a,0xb4,0xef,
-                            0xf1,0xb5,0xb0,0x9b,0x3f,0x83,0x76,0x2a,
-                            0xb9,0xda,0x5f,0x66,0x22,0x9c,0x91,0x7b,
-                            0xea,0x98,0xa6,0xe9,0x8d,0x02,0xd3,0x53 };
+const char PRIVATE_KEY[] = {0x17, 0xdd, 0x56, 0x66, 0xdc, 0x2f, 0x77, 0xf3,
+                            0x97, 0xfb, 0xda, 0x1c, 0x39, 0xd5, 0xe1, 0x91,
+                            0x46, 0x88, 0x3d, 0x0a, 0xda, 0x22, 0x52, 0x3b,
+                            0x47, 0xeb, 0x6b, 0xe1, 0x52, 0xc5, 0xea, 0x3d};
 
-const string host = INFURA_HOST;
-const string path = INFURA_PATH;
+const string host = GANACHE_HOST;
+const string path = "/";
 const string myaddr = MY_ADDRESS;
 const string contract_addr = CONTRACT_ADDRESS;
 
-Web3 web3(&host, &path);
+long accumulated_mileage = 0;
 
-void eth_send_example();
+Web3 web3(&host, &path);
 
 void setup() {
     USE_SERIAL.begin(115200);
+
+    pinMode(25, OUTPUT);
+    digitalWrite(25, LOW);
 
     for(uint8_t t = 4; t > 0; t--) {
         USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
@@ -45,18 +49,35 @@ void setup() {
 
     USE_SERIAL.println("Connected");
 
-    eth_send_example();
+    digitalWrite(25, HIGH);
+
+//    eth_send_example(15);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0)
+  {
+    char buffer[SERIAL_BUFFER_SIZE];
+    memset(buffer, 0, SERIAL_BUFFER_SIZE);
+    Serial.readBytesUntil( '\0', buffer, SERIAL_BUFFER_SIZE);
+    Serial.println(buffer);
+    eth_send_example(strtol(buffer, NULL, 10));
+    // if(accumulated_mileage == 0) {
+    //     eth_send_example(strtol(buffer, NULL, 10));
+    // } else {
+    //     accumulated_mileage += strtol(buffer, NULL, 10);
+    //     long mileage_to_send = accumulated_mileage;
+    //     accumulated_mileage = 0;
+    //     eth_send_example(mileage_to_send);
+    // }
+  }
 }
 
-void eth_send_example() {
+void eth_send_example(long currentMileage) {
     Contract contract(&web3, &contract_addr);
     contract.SetPrivateKey((uint8_t*)PRIVATE_KEY);
     uint32_t nonceVal = (uint32_t)web3.EthGetTransactionCount(&myaddr);
-
     uint32_t gasPriceVal = 1410065400000;
     uint32_t  gasLimitVal = 3000000;
     string toStr = CONTRACT_ADDRESS;
@@ -64,8 +85,12 @@ void eth_send_example() {
     uint8_t dataStr[100];
     memset(dataStr, 0, 100);
     string func = "addMileage(uint256)";
-    string p = contract.SetupContractData(&func, 123);
+    string p = contract.SetupContractData(&func, currentMileage);
+    USE_SERIAL.println(p.c_str());
     string result = contract.SendTransaction(nonceVal, gasPriceVal, gasLimitVal, &toStr, &valueStr, &p);
-
     USE_SERIAL.println(result.c_str());
+
+//    if(strstr(result, "error") != NULL) {
+//        accumulated_mileage += currentMileage;
+//    }
 }
