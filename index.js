f3b7c35371db570fe0a6a90d9fd591096543fb1a
Ivan Abadzhiev <abadzhievltd@gmail.com>
2018-09-14 11:56:32 +0300

    Added all endpoints

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const app = express()
+const path = require("path")
+
+app.use(express.static(__dirname + '/css'))
 
 app.get('/', (req, res) => {
-	res.send('Search..');
+	res.sendFile(path.join(__dirname + '/index.html'))
 })
 
 app.get('/car/:vin', (req, res) => {
-	res.send('car by vin')
+	res.sendFile(path.join(__dirname + '/car.html'))
 })
 
 app.get('/register', (req, res) => {
-	res.send('register a car')
+	res.sendFile(path.join(__dirname + '/register.html'))
 })
 
 app.post('/register', (req, res) => {
