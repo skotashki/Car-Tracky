5eab56a27463a4cd69163d3177bda092fdb6cd37
Ivan Abadzhiev <abadzhievltd@gmail.com>
2018-09-14 13:12:28 +0300

    rafactoring for express

--- Services/CarService.js
+++ Services/CarService.js
@@ -3,6 +3,7 @@ const ContractInfo = require('../Config/ContractInfo');
 
 let contractAddress = ContractInfo.getContractAddress();
 let provider = ethers.providers.JsonRpcProvider("http://35.242.225.96:8545")
+
 function searchCar(vin) {
 
 }
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
 const path = require("path")
-const CarService = require('./Services/CarService');
+// const CarService = require('./Services/CarService');
 
-app.use(express.static(__dirname + '/css'))
+//test
+// const Web3 = require('web3');
+// const web3 = new Web3("http://35.242.225.96:8545")
+
+// web3.eth.getBlockNumber().then(console.log)
+
+app.use(express.static('public'))
+app.use(express.static('node_modules'))
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname + '/index.html'))
@@ -11,7 +18,7 @@ app.get('/', (req, res) => {
 
 app.get('/car/:vin', (req, res) => {
 	//TODO: PROVIDE REAL VIN HERE
-	CarService.searchCar("test");
+	// CarService.searchCar("test");
 	res.sendFile(path.join(__dirname + '/car.html'))
 })
 
